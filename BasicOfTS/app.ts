df60d735c88ca302b2718425e8117f898537d09f
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2022-11-12 01:34:08 -0500

    js types vs ts types

--- BasicOfTS/app.ts
+++ BasicOfTS/app.ts
@@ -1,9 +1,12 @@
-function add(n1, n2){
+function add(n1: number, n2: number){
+    if(typeof typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error ('Incorrect input!');
+    }
     return n1 + n2;
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
 
-add(number1, number2);
-console.log(add(number1, number2));
+const result = add(number1, number2);
+console.log(result);
